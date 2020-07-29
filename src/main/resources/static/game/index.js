fc0ac24a27bda5cc10389c3a2e5b1461edb24bfa
little5words <little5words@users.noreply.github.com>
2020-07-29 14:35:54 -0500

    work so far

--- src/main/resources/static/game/index.js
+++ src/main/resources/static/game/index.js
@@ -1,82 +1,118 @@
-let sprites;
-
-let lastMovement="";
-let username;
-let myUserEnityId;
-
-let currentBoardName;
-let boardWidth;
-let boardHeight;
+let grass, door, boxSpawn,goalBarrier,guideSpawn,water;
+let guideStand,chest,playerStand; let grassDead;
+let sprites;    let lastMovement="";    let username;
+let myUserEnityId; let i,g=0,gd=0;
+let currentBoardName,boardWidth, boardHeight;
 let charAlias = new Map();
-let boardMap;
-let entitySprites;
-
+let boardMap;   let entitySprites;
+let animatedPlayerUp, animatedPlayerDown, animatedPlayerLeft, animatedPlayerRight;
 TILE_SIZE = 60;
-WINDOW_SIZE = 30 * TILE_SIZE;
-let app;
-let container;
-
+WINDOW_SIZE = 20 * TILE_SIZE;
 let boardInfoURL = '/board';
 
-function init(){ // called on startup
-    //getting images TODO: search for pixi texture loading
+let app = new PIXI.Application({ //make transparent later to look better ---level 0
+    width: WINDOW_SIZE, height: WINDOW_SIZE, transparent:true
+});
+document.body.appendChild(app.view);
+let container = new PIXI.Container;
+app.stage.addChild(container);
+PIXI.loader.add("/game/landscape-sheet.json")
+    .add("/game/guide-sheet.json")
+    .add("/game/box-sheet.json")
+    .add("/game/player-sheet.json")
+    .load(setupSprites);
+//5 = highest priority & 0 = lowest priority & !=done
+
+function setupSprites() {
+    let landscape = PIXI.loader.resources["/game/landscape-sheet.json"].spritesheet;
+    door = new PIXI.Sprite(landscape.textures["door.png"]);
+    boxSpawn = new PIXI.Sprite(landscape.textures["box-spawn.png"]);
+    goalBarrier = new PIXI.Sprite(landscape.textures["goal-barrier.png"]);
+    guideSpawn = new PIXI.Sprite(landscape.textures["guide-spawn.png"]);
+    water = new PIXI.Sprite(landscape.textures["water.png"]);
+
+    //make standstill sprite for guide! possible walk animation?? is it needed?----- level 1
+    let guide = PIXI.loader.resources["/game/guide-sheet.json"].spritesheet;
+    guideStand = new PIXI.Sprite(guide.textures["guide_down_stand.png"]);
+
+    //make sprite for chest(closed)!, animation open and stay open!, make animation only happen when hit ---level 2
+    let box = PIXI.loader.resources["/game/box-sheet.json"].spritesheet;
+    chest = new PIXI.Sprite(box.textures["box_1.png"]);
+    /**
+     * //animation for chest start chest closed, mid, then open and stay open
+     //make animation only happen when "hit"
+     * animatedChest= new PIXI.AnimatedSprite(box.animations["box"]);
+     *     animatedChest.animationSpeed = 0.011;
+     *     animatedChest.play();
+     *     animatedChest.loop=false;
+     *     app.stage.addChild(animatedChest);
+     */
+        //make standstill player sprite!, make walk animations!, tie the animations to the key inputs --- level 3
+    let player = PIXI.loader.resources["/game/player-sheet.json"].spritesheet;
+    playerStand = new PIXI.Sprite(player.textures["main_down_stand.png"]);
+    //need to tie each walk animation for each direction input
+    /**
+     let animatedPlayerLeft= new PIXI.AnimatedSprite(player.animations["main_left_walk"]);
+     animatedPlayerLeft.animationSpeed = 0.167;
+     animatedPlayerLeft.play();
+     let animatedPlayerRight= new PIXI.AnimatedSprite(player.animations["main_right_walk"]);
+     animatedPlayerRight.animationSpeed = 0.167;
+     animatedPlayerRight.play();
+     let animatedPlayerUp= new PIXI.AnimatedSprite(player.animations["main_up_walk"]);
+     animatedPlayerUp.animationSpeed = 0.167;
+     animatedPlayerUp.play();
+     let animatedPlayerDown= new PIXI.AnimatedSprite(player.animations["main_down_walk"]);
+     animatedPlayerDown.animationSpeed = 0.167;
+     animatedPlayerDown.play();
+    **/
     sprites = [];
-    sprites["water"] = document.getElementById("water");
-    sprites["grassDead"] = document.getElementById("grassDead");
-    sprites["grass"] = document.getElementById("grass");
-    sprites["door"] = document.getElementById("door");
-    sprites["npc"] = document.getElementById("GuideSprite");
-    sprites["OtherPlayerSprite"] = document.getElementById("OtherPlayerSprite");
-    sprites["player"] = document.getElementById("PlayerSprite");
-    document.onkeydown = updateKeys;//gets key presses
+    sprites["door"] = door;
+    sprites["player"] = playerStand;
+    document.onkeydown = updateKeys;
+
+    grass=[];
+    for(i=0; i<10000;i++){
+        grass[i]=new PIXI.Sprite(landscape.textures["grass.png"]);
+        grass[i].height=TILE_SIZE;
+        grass[i].width=TILE_SIZE;
+        container.addChild(grass[i]);
+    }
+
+    grassDead=[];
+    for(i=0; i<100000;i++){
+        grassDead[i]=new PIXI.Sprite(landscape.textures["grass_dead.png"]);
+        grassDead[i].height=TILE_SIZE;
+        grassDead[i].width=TILE_SIZE;
+        container.addChild(grassDead[i]);
+    }
 
     charAlias = [];
-    charAlias["-"] = "grass";
     charAlias[" "] = "grass";
-    charAlias["X"] = "grass";
-    charAlias["⚐"] = "grass";
-    charAlias["B"] = "grass";
     charAlias["#"] = "grassDead";
-    charAlias["@"] = "door";
     charAlias["*"] = "door";
-    charAlias["%"] = "grass";
 
     entitySprites = [];
-
-    app = new PIXI.Application({
-        width: WINDOW_SIZE, height: WINDOW_SIZE,
-        backgroundColor: 0x9999bb
-    });
-
-    document.body.appendChild(app.view);
-    container = new PIXI.Container();
-    app.stage.addChild(container);
     // retrieve username
-    username= $($.find('h1')[0]).html();
-
+    username = $($.find('h1')[0]).html();
     // connect to STOMP
     var socket = new SockJS('/WebSocketConfig');//connection link
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-
         stompClient.subscribe('/topic/event', function (message) {
             eventReaction(JSON.parse(message.body));
         });
-
         // determine player avatar and draw board
-        $.getJSON("/player-avatar/"+username,function (entity) {
-            myUserEnityId= entity.id;
+        $.getJSON("/player-avatar/" + username, function (entity) {
+            myUserEnityId = entity.id;
             loadBoard(entity.board);
         });
     });
 }
 // updates currentKey with the latest key pressed.
 function updateKeys(e){
-
     let currentKey = e.key;
     switch (currentKey){
-
         case "a":
         case "A":
         case "ArrowLeft":
@@ -84,7 +120,6 @@ function updateKeys(e){
             currentKey = null;
             lastMovement="WEST";
             break;
-
         case "d":
         case "D":
         case "ArrowRight":
@@ -92,7 +127,6 @@ function updateKeys(e){
             currentKey = null;
             lastMovement="EAST";
             break;
-
         case "w":
         case "W":
         case "ArrowUp":
@@ -100,7 +134,6 @@ function updateKeys(e){
             currentKey = null;
             lastMovement="NORTH";
             break;
-
         case "s":
         case "S":
         case "ArrowDown":
@@ -108,28 +141,21 @@ function updateKeys(e){
             currentKey = null;
             lastMovement="SOUTH";
             break;
-
         case "e":
         case "E":
             sendCommand("INTERACT", lastMovement.toString());
-            // I have to string above just to ensure that the send is all string. just in case.
             currentKey=null;
             break;
     }
-
 } // end updateKeys
-
 function loadBoard(boardName){
     $.getJSON(boardInfoURL+'/'+boardName, function(board){
-        // first clear the board
-        container.removeChildren();
-
+        container.removeChildren();// first clear the board
         // load board details
         currentBoardName = boardName;
         boardWidth = board.width+1;
         boardHeight = board.height;
         boardMap = board.tilemap;
-
         console.log(boardMap);
         console.log(currentBoardName);
         // create board tiles
@@ -140,21 +166,29 @@ function loadBoard(boardName){
                     case "\n":
                         pos++;
                         break;
-                    default:
-                        tileImage = sprites[charAlias[boardMap.charAt(pos)]];
-                        const texture = PIXI.Texture.from(tileImage);
-                        const tile = new PIXI.Sprite(texture);
-                        tile.height = TILE_SIZE;
-                        tile.width = TILE_SIZE;
+                    case " ":
+                        tile= grass[g];
                         tile.x = ix * TILE_SIZE;
                         tile.y = iy * TILE_SIZE;
                         container.addChild(tile);
-
+                        pos++; g++;break;
+                    case "#":
+                        tile= grassDead[gd];
+                        tile.x = ix * TILE_SIZE;
+                        tile.y = iy * TILE_SIZE;
+                        container.addChild(tile);
+                        pos++; gd++;break;
+                    default:
+                        tileImage = sprites[charAlias[boardMap.charAt(pos)]];
+                        tileImage.height = TILE_SIZE;
+                        tileImage.width = TILE_SIZE;
+                        tileImage.x = ix * TILE_SIZE;
+                        tileImage.y = iy * TILE_SIZE;
+                        container.addChild(tileImage);
                         pos++;
                 }
             }
-        }
-        // add entities on the tile (if any)
+        }// add entities on the tile (if any)
         $.getJSON('/container/board/'+currentBoardName,
             function(entityIds) {
                 entityIds.forEach(function (id) {
@@ -166,7 +200,6 @@ function loadBoard(boardName){
         );
     });
 }
-
 function drawEntity(entity){
     if(entitySprites[entity.id]) { // entity has a sprite
         sprite = entitySprites[entity.id];
@@ -176,18 +209,14 @@ function drawEntity(entity){
         container.addChild(sprite);
     } else {
         entityImage = sprites[entity.type];
-        const texture = PIXI.Texture.from(entityImage);
-        const sprite = PIXI.Sprite.from(texture);
-        sprite.x = entity.column * TILE_SIZE;
-        sprite.y = entity.row * TILE_SIZE;
-        sprite.height = TILE_SIZE;
-        sprite.width = TILE_SIZE;
-        container.addChild(sprite);
-        entitySprites[entity.id] = sprite;
+        entityImage.x = entity.column * TILE_SIZE;
+        entityImage.y = entity.row * TILE_SIZE;
+        entityImage.height = TILE_SIZE;
+        entityImage.width = TILE_SIZE;
+        container.addChild(entityImage);
+        entitySprites[entity.id] = entityImage;
     }
 }
-
-
 // ***** The following methods display 'debugging'    *****
 // ***** information that's retrieved from the server *****
 //sends a command
@@ -199,14 +228,11 @@ function sendCommand(command, parameter) {
         }
     ));
 }
-
 function eventReaction(event) {
-
     switch (event.type) {
         case "entity-created":
         case "entity-moved":
             $.getJSON("/entity/"+event.properties.entity,function (entity) {
-                console.log('AKSJFHASKJHGASKJGHASKJGH');
                 console.log(entity);
                 // check to see if it is the current player's avatar
                 if (entity.properties.player!=undefined){
@@ -221,15 +247,11 @@ function eventReaction(event) {
                     drawEntity(entity);
                 }
             });
-
             break;
-
         case "speech":
             window.alert(event.properties.message);
             break;
-
-        case "command":
-            //ignore
+        case "command"://ignore
             break;
         default:
     }
